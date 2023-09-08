fdf5f9089d82eade22df5136abb828ab0c0659e1
GYALLERHORN <kjins1998@gmail.com>
2023-09-08 17:47:54 +0900

    중간 커밋

--- SnackCrash/Assets/Script/DataManagement/StageManager.cs
+++ SnackCrash/Assets/Script/DataManagement/StageManager.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class StageManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject Canvas;
+
+    private void Start()
     {
-        
+
+        if (!PlayerPrefs.HasKey("playerLevel") && !PlayerPrefs.HasKey("stageLevel"))
+        {
+            PlayerPrefs.SetInt("playerLevel", 0);
+            PlayerPrefs.SetInt("stageLevel", 1);
+        }
+        Debug.Log(PlayerPrefs.GetInt("playerLevel"));
+
+        CheckStage(PlayerPrefs.GetInt("playerLevel"));
     }
 
-    // Update is called once per frame
-    void Update()
+    void CheckAccessible(GameObject gameObject)
     {
-        
+
+    }
+
+    void CheckStage(int playerLevel)
+    {
+        for (int i = playerLevel; i < 4; i++)
+        {
+            if (i + 1 < PlayerPrefs.GetInt("stageLevel"))
+            {
+                LockStage(PlayerPrefs.GetInt("stageLevel"));
+            }
+        }
+    }
+
+    void LockStage(int stageLevel)
+    {
+        Canvas.transform.Find($"Stage{stageLevel}LockPanel").gameObject.SetActive(true);
+        Canvas.transform.Find($"Stage{stageLevel}").gameObject.SetActive(false);
     }
 }
