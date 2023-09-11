ff613086e287bae38804ee69b411a934510e013f
jhwan328 <jhwan328@naver.com>
2023-09-11 16:00:43 +0900

    Paddle 콜라이더 세분화
    5개의 collider로 나누고 각각 tag를 달아 ball2가 충돌 시 다른 방향으로 이동하도록 설정.
    아이템이 블록 위에 머무르는 현상 수정

--- SnackCrash/Assets/Script/Ball/Ball_2.cs
+++ SnackCrash/Assets/Script/Ball/Ball_2.cs
@@ -7,23 +7,56 @@ public class Ball_2 : MonoBehaviour
     private Rigidbody2D rb;
     public float Movement_x=30.0f;
     public float Movement_y=30.0f;
+    public float time =0;
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
     private void Update()
     {
+        time += Time.deltaTime;
         rb.velocity = new Vector3(Movement_x, Movement_y,0);
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.CompareTag("RightWall")|| collision.gameObject.CompareTag("LeftWall"))
+        if (collision.gameObject.CompareTag("RightWall") || collision.gameObject.CompareTag("LeftWall"))
         {
             Movement_x *= -1;
         }
-        if (collision.gameObject.CompareTag("Top") || collision.gameObject.CompareTag("Bottom")|| collision.gameObject.CompareTag("Paddle") || collision.gameObject.CompareTag("Block"))
+        else if (collision.gameObject.CompareTag("Top") || collision.gameObject.CompareTag("Bottom") || collision.gameObject.CompareTag("Paddle") || collision.gameObject.CompareTag("Block"))
         {
             Movement_y *= -1;
+        }else if (time < 0.1f)
+        {
+            return;
         }
+        else if (collision.gameObject.CompareTag("Paddle_Low"))
+        {
+            float sign = Mathf.Sign(Movement_x);
+            float angleInRadians = 45f * Mathf.Deg2Rad;
+            float speed = Mathf.Sqrt(Movement_x * Movement_x + Movement_y * Movement_y);
+            Movement_x = speed * Mathf.Cos(angleInRadians)*sign;
+            Movement_y = speed * Mathf.Sin(angleInRadians);
+            time = 0;
+        }
+        else if (collision.gameObject.CompareTag("Paddle_High"))
+        {
+            float sign = Mathf.Sign(Movement_x);
+            float angleInRadians = 75f * Mathf.Deg2Rad;
+            float speed = Mathf.Sqrt(Movement_x * Movement_x + Movement_y * Movement_y);
+            Movement_x = speed * Mathf.Cos(angleInRadians) * sign;
+            Movement_y = speed * Mathf.Sin(angleInRadians);
+        }
+        else if (collision.gameObject.CompareTag("Paddle_Mid"))
+        {
+            Movement_y *= -1;
+            time = 0;
+        }
+        else
+        {
+            // 다른 태그에 대한 처리
+        }
+
+
     }
 }
